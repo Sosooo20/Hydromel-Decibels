b31da67dcdca4cbe6cd8eb859c7420f927818cb7
gunter4903 <antoine.fungere.27@gmail.com>
2026-06-11 15:39:33 +0200

    Add adress IP

--- prisma/migrations/20260610124136_add_hours_to_stand_activity/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "Activity" ADD COLUMN     "endTime" TEXT,
-ADD COLUMN     "startTime" TEXT;
-
--- AlterTable
-ALTER TABLE "Stand" ADD COLUMN     "closeTime" TEXT,
-ADD COLUMN     "openTime" TEXT;
